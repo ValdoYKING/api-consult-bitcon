4d184c56a6f580b15f4d4ec87e347a8f52a767b0
ValdoYKING <232271007@alumnos.utn.edu.mx>
2023-07-12 16:00:36 -0600

    Cambios sobre las urls

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(cors());
 
 // Configuración CORS
 app.use(cors({
-    origin: 'https://unrivaled-smakager-8d710f.netlify.app'
+    // origin: 'https://unrivaled-smakager-8d710f.netlify.app'
+    origin: ['*','https://unrivaled-smakager-8d710f.netlify.app']
 }));
 
 // routes
